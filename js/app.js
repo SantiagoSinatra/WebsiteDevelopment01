aa1e35c19a9e0a3777869b8dae2db9e9d5e010b0
DevelopeandoHub <contacto.developeando@gmail.com>
2022-10-12 11:06:26 -0300

    added first part of carousel layout and functionality

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
 window.onload = () => {
-    console.log(123);
+    let i = 0;
+    let images = ["placeholder.jpg"];
+    let time = 3000;
+    let carousel = document.getElementById("carousel");
+
+    function changeImage() {
+        carousel.src = `img/${images[i]}`;
+        if(i < images.length - 1){
+            i++
+        } else {
+            i = 0;
+        }
+    }
+
+    changeImage();
 }
