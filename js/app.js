181d5867429789a98d5d77a0d9e1004f0ec35998
DevelopeandoHub <contacto.developeando@gmail.com>
2022-10-12 17:23:30 -0300

    added complete carousel functionality

--- js/app.js
+++ js/app.js
@@ -1,16 +1,27 @@
 window.onload = () => {
     let i = 0;
-    let images = ["placeholder.jpg"];
-    let time = 3000;
+    let images = ["placeholder.jpg", "placeholder2.jpg"];
+    let time = 5000;
     let carousel = document.getElementById("carousel");
 
+    function changeSrc(delay) {
+        i < images.length - 1 ? i++ : i = 0;
+        setInterval(() => {
+            carousel.src = `img/${images[i]}`;
+        }, delay);
+    }
+
+    function toggleOpacity(delay) {
+        carousel.style.opacity = 0;
+        setInterval(() => {
+            carousel.style.opacity = 1;
+        }, delay);
+    }
+
     function changeImage() {
-        carousel.src = `img/${images[i]}`;
-        if(i < images.length - 1){
-            i++
-        } else {
-            i = 0;
-        }
+        toggleOpacity(1000);
+        changeSrc(500);
+        setTimeout(changeImage, time);
     }
 
     changeImage();
